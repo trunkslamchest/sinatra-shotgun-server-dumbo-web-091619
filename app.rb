0da79e4af9e398382483f7e432f2138c0af30cd7
Austin Smith <trunk9slamchest@gmail.com>
2019-10-07 14:11:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
-require_relative 'config/environment'
+class Application < Sinatra::Base
 
-class App < Sinatra::Base
-
-  get '/' do 
-    "Welcome to your app!!!! "
-  end
+	get '/' do
+		"Hello, World!"
+	end
 
 end
